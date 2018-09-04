e11d10e7e4b2c16193b264506dcf6188255c6f9e
Gaurav Sofat <gauravsofat12@gmail.com>
2018-09-04 19:49:23 +0530

    Candidate Model Created

---
+++ models/candidate.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const candidateSchema = new Schema(
+  {
+    comName: String, // Comittee For Which Candidate Is Contesting
+    name: String, // Name Of Candidate
+    sid: String, // Unique Student ID
+    cpi: Number // Cumalative Performance Index
+  },
+  { collection: "candidates" }
+);
+
+// Create A Virtual Attribute Batch From A Substring Of 'sid'
+candidateSchema.virtual("batch").get(function() {
+  return Number(this.sid.toString().substring(2, 6));
+});
+
+module.exports = mongoose.model("Candidate", candidateSchema);
